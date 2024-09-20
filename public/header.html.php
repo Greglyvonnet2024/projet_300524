5eb5e87c585bcce804e162db592eb5b15f739701
Greg <gregorylyvonet@lepoles.org>
2024-09-20 15:17:35 +0200

    Initial Commit

---
+++ public/header.html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+    <link rel="stylesheet" href="../htdocs/projet_300524/">
+
+</head>
+
+<body>
+    
+
+
+<?php include __DIR__ . "public/asset/css/style.css" 
